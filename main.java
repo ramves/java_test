9afa0901b7327267f4bd40505a84a1a0c42832a9
ramves <d2025.ram.panjwani@ves.ac.in>
2025-09-19 13:55:58 +0530

    Second Commit

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
+        System.out.println("This is a simple Java program.");
     }
 }

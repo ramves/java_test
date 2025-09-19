f806ca5f900a04d21c224d3b8f06bc4032ccb4f0
ramves <d2025.ram.panjwani@ves.ac.in>
2025-09-19 14:05:03 +0530

    Third Commit

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
         System.out.println("This is a simple Java program.");
+        System.out.println("It prints multiple lines to the console.");
     }
 }
